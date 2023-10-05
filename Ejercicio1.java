be7222e10b27cf4aa0f475aaeb4b84d9dc4dd052
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-05 10:16:56 +0200

    capitulo 2

---
+++ Ejercicio1.java
@@ -0,0 +1,22 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int x = 144;
+        int y = 999;
+        int suma;
+        int resta;
+        double division;
+        int multiplicacion;
+
+        suma = 144+999;
+        resta = 144-199;
+        multiplicacion = 144*999;
+        division = (double)144/999;
+
+
+        System.out.println(x + " + " + y + " = " + suma);
+        System.out.println(x + " - " + y + " = " + resta);
+        System.out.println(x + " * " + y + " = " + multiplicacion);
+        System.out.println(x + " / " + y + " = " + division);
+
+    }
+}
