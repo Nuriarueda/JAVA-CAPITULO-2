3e6b9105a9025a0fdad13c7ffc91d44a79cb6ba8
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-05 10:23:25 +0200

    capitulo 2

---
+++ Ejercicio3.java
@@ -0,0 +1,13 @@
+/* Crea las variables nombre, direccion y telefono y asígnales los valores correspondientes. Muestra los valores de esas variables por pantalla de tal forma que el
+resultado del programa sea el mismo que en el ejercicio 2. */
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        String nombre = "Nuria Rueda Reina";
+        String direccion = "Calle Limonero 33";
+        String telefono = "672 34 56 78";
+
+        System.out.println(nombre);
+        System.out.println(direccion);
+        System.out.println(telefono);
+    }
+}
