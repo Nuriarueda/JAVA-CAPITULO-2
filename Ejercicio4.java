603254daedda2af432ca9b38195e538c0bb9e3eb
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-05 10:37:00 +0200

    capitulo 2

---
+++ Ejercicio4.java
@@ -0,0 +1,11 @@
+/* Realiza un conversor de euros a pesetas. La cantidad en euros que se quiere
+convertir deberá estar almacenada en una variable. */
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        
+        System.out.println("CONVERSOR DE EUROS A PESETAS");
+        int euros = 20;
+
+        System.out.println(euros + " euros son " + (euros*166.386) + " pesetas");
+    }
+}
---
+++ Ejercicio5.java
@@ -0,0 +1,13 @@
+/* Realiza un conversor de pesetas a euros. La cantidad en pesetas que se quiere
+convertir deberá estar almacenada en una variable. */
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        
+        System.out.println("CONVERSOR DE PESETAS A EUROS");
+        int pesetas = 100;
+
+        System.out.println(pesetas + " pesetas son " + (pesetas*0.00601012 ) + " pesetas");
+    }
+
+}
+
