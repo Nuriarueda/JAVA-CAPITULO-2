2aea918268d3687cc19b3f88efd528faff2cbe26
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-05 10:20:40 +0200

    capitulo 2

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,3 +1,6 @@
+/* Escribe un programa en el que se declaren las variables enteras x e y. Asígnales
+los valores 144 y 999 respectivamente. A continuación, muestra por pantalla
+el valor de cada variable, la suma, la resta, la división y la multiplicación.*/
 public class Ejercicio1 {
     public static void main(String[] args) {
         int x = 144;
